615bfca7b651426c20539047df272e6b67d34b82
4ross <a.rosselli11@gmail.com>
2020-02-26 21:23:06 +0100

    Aggiunta gestione RTC

--- main/main.ino
+++ main/main.ino
@@ -2,6 +2,7 @@
 #include <LiquidCrystal.h>
 #include <Arduino_MKRTHERM.h>
 #include <WiFiNINA.h>
+#include <RTCZero.h>
 
 #include "arduino_secrets.h"
 // initialize the library by associating any needed LCD interface pin
@@ -17,6 +18,21 @@ int keyIndex = 0;                 // your network key Index number (needed only 
 int status = WL_IDLE_STATUS;
 WiFiServer server(80);
 IPAddress ip;
+
+/* Create an rtc object */
+RTCZero rtc;
+
+/* Change these values to set the current initial time */
+const byte seconds = 0;
+const byte minutes = 00;
+const byte hours = 17;
+
+/* Change these values to set the current initial date */
+const byte day = 17;
+const byte month = 11;
+const byte year = 15;
+
+float thermoCouple = 0.0;
 
 void setup() {
   analogWrite(A3, 50); // Set the brightness to its maximum value
@@ -64,25 +80,26 @@ void setup() {
   server.begin();                           // start the web server on port 80
   wifiStatus();                        // you're connected now, so print out the status
 
+  rtc.begin();
+
+  rtc.setTime(hours, minutes, seconds);
+  rtc.setDate(day, month, year);
+  rtc.setAlarmSeconds(seconds + 10);
+  rtc.enableAlarm(rtc.MATCH_SS);
+
+  rtc.attachInterrupt(alarmMatch);
 }
 void loop() {
-  float thermoCouple = 0.0;
+
   //WIFI
   WiFiClient client = server.available();   // listen for incoming clients
-  
-  thermoCouple = THERM.readTemperature();
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.clear();
-  lcd.print(ip);
-  lcd.setCursor(0, 1);
-  lcd.print("T: ");
-  lcd.print(thermoCouple);
-  lcd.print(" C");
 
 
-  if (client) {         
-    newWifiClient(client,thermoCouple);
+
+
+
+  if (client) {
+    newWifiClient(client, thermoCouple);
   }
   delay(500);
 }
@@ -99,45 +116,83 @@ void wifiStatus() {
 
 void newWifiClient(WiFiClient client, float thermoCouple )
 {
-      // if you get a client,
-    lcd.clear();
-    lcd.print("new client");
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        if (c == '\n') {                    // if the byte is a newline character
+  // if you get a client,
+  lcd.clear();
+  lcd.print("new client");
+  String currentLine = "";                // make a String to hold incoming data from the client
+  while (client.connected()) {            // loop while the client's connected
+    if (client.available()) {             // if there's bytes to read from the client,
+      char c = client.read();             // read a byte, then
+      if (c == '\n') {                    // if the byte is a newline character
 
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");  // the connection will be closed after completion of the response
-            client.println();
-            client.print("T: ");
-            client.print(thermoCouple);
-            // The HTTP response ends with another blank line:
-            //client.println();
-            // break out of the while loop:
-            break;
-          } else {    // if you got a newline, then clear currentLine:
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
+        // if the current line is blank, you got two newline characters in a row.
+        // that's the end of the client HTTP request, so send a response:
+        if (currentLine.length() == 0) {
+          // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+          // and a content-type so the client knows what's coming, then a blank line:
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-type:text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println();
+          client.print("T: ");
+          client.print(thermoCouple);
+          // The HTTP response ends with another blank line:
+          //client.println();
+          // break out of the while loop:
+          break;
+        } else {    // if you got a newline, then clear currentLine:
+          currentLine = "";
         }
+      } else if (c != '\r') {  // if you got anything else but a carriage return character,
+        currentLine += c;      // add it to the end of the currentLine
+      }
 
-        // Check to see if the client request was "GET /H" or "GET /L":
-        if (currentLine.endsWith("GET /H")) {
+      // Check to see if the client request was "GET /H" or "GET /L":
+      if (currentLine.endsWith("GET /H")) {
 
-        }
       }
     }
-    // close the connection:
-    client.stop();
-    lcd.clear();
-    lcd.print("disconnected");
+  }
+  // close the connection:
+  client.stop();
+  lcd.clear();
+  lcd.print("disconnected");
+}
+
+void readThermocouple()
+{
+  thermoCouple = THERM.readTemperature();
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  lcd.print("T: ");
+  lcd.print(thermoCouple);
+  lcd.print(" C");
+}
+
+void alarmMatch()
+{
+  byte newAlarm = rtc.getSeconds();
+  if (newAlarm >= 59)
+  {
+    newAlarm = 0;
+  }
+  rtc.setAlarmSeconds(newAlarm);
+  lcd.clear();
+
+  print2digits(rtc.getHours());
+  lcd.print(":");
+  print2digits(rtc.getMinutes());
+  lcd.print(":");
+  print2digits(rtc.getSeconds());
+
+  lcd.setCursor(0, 1);
+  readThermocouple();
+}
+
+void print2digits(int number) {
+  if (number < 10) {
+    lcd.print("0"); // print a 0 before if the number is < than 10
+  }
+  lcd.print(number);
 }
